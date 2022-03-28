f50e8245f0bba48a9d10670eb78d13a05b93716d
lucas donato <lucas.ezequiel.sm@hotmail.com>
2022-03-28 20:24:15 -0300

    Se agrego img como pide la consigna

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -6,17 +6,17 @@ import ItemCount from "../itemcount/ItemCount";
 export default function Card({info}) {
 
     try {
-        var {titulo, talle, precio, stock} = info
+        var {img, titulo, talle, precio, stock} = info
         console.log(info)
     } catch (error) {
-        
+
     }
 
     const onAdd = (count) => { console.log(`Agregaste ${count} productos al carrito.`) }
 
     return (
         <div className="container-card">
-            <img className='card-imagen' src={FotoProducto} alt="Icono Producto"></img>
+            <img className='card-imagen' src={img} alt="Icono Producto"></img>
             <div className='container-description'>
                 <p className='description-titulo'>{titulo}</p>
                 <p className='description-talle'>{talle}</p>
--- src/components/itemlistcontainer/itemlist/ItemList.js
+++ src/components/itemlistcontainer/itemlist/ItemList.js
@@ -4,10 +4,18 @@ import Card from "../../card/Card";
 
 const ItemList = () => {
     const mockProductos = [{
+        img : "https://media.vogue.es/photos/5cc7367ffdc82261481fda65/master/w_2500,h_3194,c_limit/pantalones_vaqueros_mom_jeans_goma_pull_bear_basico_armario_2019_7512.jpg",
         titulo : "Pantalon",
         talle : "XL",
         precio : 3000,
         stock : 5
+    },
+    {
+        img: "https://www.instyle.es/medio/2019/02/04/pantalones-vaqueros-cropped-primavera-uterque_b2a5edab_1000x1499.jpg",
+        titulo : "Pantalon",
+        talle : "L",
+        precio : 2900,
+        stock : 7
     }]
 
     const [products, setProducts] = useState([])
@@ -31,6 +39,7 @@ const ItemList = () => {
     return(
         <div className="container-category-cards">
             <Card info={products[0]}/>
+            <Card info={products[1]}/>
         </div>
     )
 }
